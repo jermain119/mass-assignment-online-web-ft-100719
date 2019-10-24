2877952884f08282dc3309994d80d9439dba3afa
joshua richards <jermain119@gmail.com>
2019-10-24 23:13:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,14 @@
 class Person
   #your code here
-
-
+  
+  class User
+  attr_accessor :name, :age, :location, :user_name
+ 
+  def initialize(user_name:, name:, age:, location:)
+    @user_name = user_name
+    @name = name
+    @location = location
+    @age = age
+  end
+end
 end
